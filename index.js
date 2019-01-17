654243156bb7bb5b882ce2643a3cf0c1dff5c97b
Mike Gorton <mike.gorton1@gmail.com>
2019-01-17 00:55:23 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+var recipes = new Object({BlocParty: 'Helicopter'});
+
+function updateObjectWithKeyAndValue(obj, key, val) {
+  return Object.assign({}, obj, { [key]: val });
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, val) {
+
+
